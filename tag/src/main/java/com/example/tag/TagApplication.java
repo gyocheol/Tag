7004c705fa79d7116c4fa34d408d8608fa3883c6
gyocheol <kys04171@naver.com>
2024-06-18 16:25:18 +0900

    feat : DB Connection Completed

--- tag/src/main/java/com/example/tag/TagApplication.java
+++ tag/src/main/java/com/example/tag/TagApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
-@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})
+@EnableJpaAuditing
+@EnableAutoConfiguration(exclude={MongoAutoConfiguration.class})	// mongoDB socket setting
 public class TagApplication {
 
 	public static void main(String[] args) {
+		System.setProperty("spring.config.name", "application, application-db, application-login");
 		SpringApplication.run(TagApplication.class, args);
 	}
-
 }
